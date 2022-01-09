bdc6fba7b84be7501be5904ae349e14e20a5f25c
sohans <sohantraininglinux@gmail.com>
2022-01-09 04:02:59 +0000

    first commit at singapore

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "hostname of the server is :- `hostname`"
-echo "current date is " `date`"
+echo "current date is `date`"
+echo "git download testing is successfull& now uploading is doing, please confirm you get the update on one"
