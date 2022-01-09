4e41722a0a071636615fa7dd2911eec83bd5a9ba
sohans <sohantraininglinux@gmail.com>
2022-01-09 04:09:25 +0000

    lastest update for now

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@
 echo "hostname of the server is :- `hostname`"
 echo "current date is `date`"
 echo "git download testing is successfull& now uploading is doing, please confirm you get the update on one"
+echo "now igpnore link has been updated"
